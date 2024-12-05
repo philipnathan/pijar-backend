7f6b75e1a133379ac730f05285f1118373df43b0
philipnathan <philipnathan168@gmail.com>
2024-12-05 10:52:02 +0800

    fix: change to gorm for database orm

--- database/mysql.go
+++ database/mysql.go
@@ -1,13 +1,15 @@
 package database
 
 import (
-	"database/sql"
 	"fmt"
 	"os"
 	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-func ConnectToDatabase() (*sql.DB, error) {
+func ConnectToDatabase() (*gorm.DB, error) {
 
 	dbUser := os.Getenv("DB_USERNAME")
 	dbPassword := os.Getenv("DB_PASSWORD")
@@ -15,20 +17,14 @@ func ConnectToDatabase() (*sql.DB, error) {
 	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_DATABASE")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
 
 	maxRetries := 10
 	for i := 0; i < maxRetries; i++ {
-		db, err := sql.Open("mysql", dsn)
+		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 		if err != nil {
 			fmt.Println("Error connecting to database:", err)
-			time.Sleep(5 * time.Second)
-			continue
-		}
-
-		if err := db.Ping(); err != nil {
-			fmt.Println("Error pinging database:", err)
 			time.Sleep(5 * time.Second)
 			continue
 		}
