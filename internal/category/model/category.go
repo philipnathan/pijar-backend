b97b97ac379edb2807342d49cb108e17dffb6e53
philipnathan <philipnathan168@gmail.com>
2024-12-08 19:11:13 +0800

    feat(user): create category table. Data will be imported manually

--- internal/category/model/category.go
+++ internal/category/model/category.go
@@ -5,8 +5,7 @@ import (
 )
 
 type Category struct {
-	gorm.Model    `json:"-"`
-	ID            uint   `gorm:"primaryKey" json:"id"`
+	gorm.Model
 	Category_name string `gorm:"type:varchar(50);not null" json:"category_name"`
 	Image_url     string `gorm:"type:text" json:"image_url"`
 }
