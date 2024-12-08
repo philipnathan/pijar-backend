070d532e0a12f664e6782a547e0c4f4bbcc859c6
philipnathan <philipnathan168@gmail.com>
2024-12-09 00:21:52 +0800

    feat: add swagger setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,9 +5,11 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/philipnathan/pijar-backend/database"
+	_ "github.com/philipnathan/pijar-backend/docs"
 	categoryRoute "github.com/philipnathan/pijar-backend/internal/category/route"
 	seed "github.com/philipnathan/pijar-backend/internal/seed"
 	userRoute "github.com/philipnathan/pijar-backend/internal/user/route"
+	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
@@ -15,13 +17,8 @@ import (
 //	@version		1.0
 //	@description	This is a Pijar API
 
-//	@host		localhost:8080
-//	@BasePath	/api/v1
-
-// @securityDefinitions.apikey	Bearer
-// @in							header
-// @name						Authorization
-// @description				"Type 'Bearer TOKEN' to correctly set the API Key"
+// @host		localhost:8080
+// @BasePath	/api/v1
 func main() {
 	db, err := database.ConnectToDatabase()
 
