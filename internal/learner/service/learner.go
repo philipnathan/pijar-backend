f3c5b7fe82bf412ea38c75dc9cb484d1e37a73d6
philipnathan <philipnathan168@gmail.com>
2024-12-08 21:24:40 +0800

    feat(learner): create service, repository, and model for learner entity

--- internal/learner/service/learner.go
+++ internal/learner/service/learner.go
@@ -7,8 +7,6 @@ import (
 
 type LearnerServiceInterface interface {
 	GetLearnerInterests(userID uint) ([]model.LearnerInterest, error)
-	AddLearnerInterests(userID uint, interests []uint) error
-	DeleteLearnerInterests(userID uint, interests []uint) error
 }
 
 type LearnerService struct {
@@ -31,49 +29,7 @@ func (s *LearnerService) GetLearnerInterests(userID uint) ([]model.LearnerIntere
 }
 
 func (s *LearnerService) AddLearnerInterests(userID uint, interests []uint) error {
-	learnerInterests, err := s.repo.GetLearnerInterest(userID)
-	if err != nil {
-		return err
-	}
-
-	existingInterests := make(map[uint]bool)
-	for _, data := range learnerInterests {
-		existingInterests[data.Category.ID] = true
-	}
-
-	var newInterests []uint
-	for _, categoryID := range interests {
-		if !existingInterests[categoryID] {
-			newInterests = append(newInterests, categoryID)
-		}
-	}
-
-	if err := s.repo.AddLearnerInterests(userID, newInterests); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (s *LearnerService) DeleteLearnerInterests(userID uint, interests []uint) error {
-	learnerInterests, err := s.repo.GetLearnerInterest(userID)
-	if err != nil {
-		return err
-	}
-
-	existingInterests := make(map[uint]bool)
-	for _, data := range learnerInterests {
-		existingInterests[data.Category.ID] = true
-	}
-
-	var deleteInterests []uint
-	for _, categoryID := range interests {
-		if existingInterests[categoryID] {
-			deleteInterests = append(deleteInterests, categoryID)
-		}
-	}
-
-	if err := s.repo.DeleteLearnerInterests(userID, deleteInterests); err != nil {
+	if err := s.repo.AddLearnerInterests(userID, interests); err != nil {
 		return err
 	}
 
