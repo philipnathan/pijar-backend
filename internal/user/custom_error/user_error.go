9bb0548fcd74d17da20488c28359ab35b85c534b
philipnathan <philipnathan168@gmail.com>
2024-12-09 00:22:16 +0800

    feat(user): create documentation for user register route

--- internal/user/custom_error/user_error.go
+++ internal/user/custom_error/user_error.go
@@ -3,12 +3,16 @@ package user
 import "errors"
 
 var (
-	ErrUserNotFound = errors.New("user not found")
-	ErrUserExist = errors.New("user already exist")
+	ErrUserNotFound     = errors.New("user not found")
+	ErrUserExist        = errors.New("user already exist")
 	ErrPhoneNumberExist = errors.New("phone number already exist")
-	ErrEmailExist = errors.New("email already exist")
-	ErrLogin = errors.New("invalid email or password")
-	ErrToken = errors.New("invalid token")
-	ErrWrongPassword = errors.New("wrong password")
-	ErrSamePassword = errors.New("new password cannot be the same as old password")
+	ErrEmailExist       = errors.New("email already exist")
+	ErrLogin            = errors.New("invalid email or password")
+	ErrToken            = errors.New("invalid token")
+	ErrWrongPassword    = errors.New("wrong password")
+	ErrSamePassword     = errors.New("new password cannot be the same as old password")
 )
+
+type Error struct {
+	Error string `json:"error" example:"user not found"`
+}
