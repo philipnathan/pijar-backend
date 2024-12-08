8e329157382006a2d3cd78a6ef54227f97917b10
philipnathan <philipnathan168@gmail.com>
2024-12-08 21:24:40 +0800

    feat(learner): create service, repository, and model for learner entity

---
+++ internal/learner/service/learner.go
@@ -0,0 +1,37 @@
+package learner
+
+import (
+	model "github.com/philipnathan/pijar-backend/internal/learner/model"
+	repo "github.com/philipnathan/pijar-backend/internal/learner/repository"
+)
+
+type LearnerServiceInterface interface{
+	GetLearnerInterests(userID uint) ([]model.LearnerInterest, error)
+}
+
+type LearnerService struct {
+	repo repo.LearnerRepositoryInterface
+}
+
+func NewLearnerService(repo repo.LearnerRepositoryInterface) LearnerServiceInterface {
+	return &LearnerService{
+		repo: repo,
+	}
+}
+
+func (s *LearnerService) GetLearnerInterests(userID uint) ([]model.LearnerInterest, error) {
+	var learnerInterest []model.LearnerInterest
+	learnerInterest, err := s.repo.GetLearnerInterest(userID)
+	if err != nil {
+		return nil, err
+	}
+	return learnerInterest, nil
+}
+
+func (s *LearnerService) AddLearnerInterests(userID uint, interests []uint) error {
+	if err := s.repo.AddLearnerInterests(userID, interests); err != nil {
+		return err
+	}
+
+	return nil
+}
