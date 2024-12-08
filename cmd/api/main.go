8000b4f424b867be07d4ca9b797e6ecb1481cd23
philipnathan <philipnathan168@gmail.com>
2024-12-09 00:21:52 +0800

    feat: add swagger setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,11 +5,20 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/philipnathan/pijar-backend/database"
+	_ "github.com/philipnathan/pijar-backend/docs"
 	categoryRoute "github.com/philipnathan/pijar-backend/internal/category/route"
 	seed "github.com/philipnathan/pijar-backend/internal/seed"
 	userRoute "github.com/philipnathan/pijar-backend/internal/user/route"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+//	@title			Pijar API
+//	@version		1.0
+//	@description	This is a Pijar API
+
+//	@host		localhost:8080
+//	@BasePath	/api/v1
 func main() {
 	db, err := database.ConnectToDatabase()
 
@@ -22,6 +31,7 @@ func main() {
 	database.MigrateDatabase(db)
 
 	r := gin.Default()
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	if err := seed.SeedDatabase(db); err != nil {
 		fmt.Println("Failed to seed database:", err)
