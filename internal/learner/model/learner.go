7e49cb99c8427e5decfc76d3b966763829cff7f6
philipnathan <philipnathan168@gmail.com>
2024-12-08 21:24:40 +0800

    feat(learner): create service, repository, and model for learner entity

---
+++ internal/learner/model/learner.go
@@ -0,0 +1,22 @@
+package learner
+
+import (
+	category "github.com/philipnathan/pijar-backend/internal/category/model"
+	"gorm.io/gorm"
+)
+
+type LearnerBio struct {
+	gorm.Model
+	UserID uint `gorm:"not null" json:"user_id"`
+	Bio string `gorm:"type:text;not null" json:"bio"`
+	Occupation string `gorm:"type:varchar(50);not null" json:"occupation"`
+	Institution string `gorm:"type:varchar(50);not null" json:"institution"`
+}
+
+type LearnerInterest struct {
+	gorm.Model
+	UserID uint `gorm:"not null" json:"user_id"`
+	CategoryID uint `gorm:"type:varchar(50);not null" json:"category_id"`
+
+	Category category.Category `gorm:"foreignKey:CategoryID;references:ID" json:"category"`
+}
---
+++ internal/learner/repository/learner.go
@@ -0,0 +1,46 @@
+package learner
+
+import (
+	model "github.com/philipnathan/pijar-backend/internal/learner/model"
+	"gorm.io/gorm"
+)
+
+type LearnerRepositoryInterface interface{
+	GetLearnerInterest(userID uint) ([]model.LearnerInterest, error)
+	AddLearnerInterests(userID uint, interests []uint) error
+}
+
+type LearnerRepository struct {
+	db *gorm.DB
+}
+
+func NewLearnerRepository(db *gorm.DB) LearnerRepositoryInterface {
+	return &LearnerRepository{
+		db: db,
+	}
+}
+
+func (r *LearnerRepository) GetLearnerInterest(userID uint) ([]model.LearnerInterest, error) {
+	var learnerInterest []model.LearnerInterest
+
+	err := r.db.Where("user_id = ?", userID).Find(&learnerInterest).Error
+	if err != nil {
+		return nil, err
+	}
+	return learnerInterest, nil
+}
+
+func (r *LearnerRepository) AddLearnerInterests(userID uint, interests []uint) error {
+
+	var learnerInterests []model.LearnerInterest
+	for _, interestID := range interests {
+		learnerInterests = append(learnerInterests, model.LearnerInterest{UserID: userID, CategoryID: interestID})
+	}
+
+	err := r.db.CreateInBatches(learnerInterests, 100).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
