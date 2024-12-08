f632268db7cf458e9ead415b7d5ee166c9f8ff08
philipnathan <philipnathan168@gmail.com>
2024-12-08 14:37:37 +0800

    fix: change package name

--- internal/user/service/user.go
+++ internal/user/service/user.go
@@ -5,8 +5,8 @@ import (
 	"reflect"
 
 	"github.com/philipnathan/pijar-backend/internal/user/custom_error"
-	"github.com/philipnathan/pijar-backend/internal/user/model"
-	"github.com/philipnathan/pijar-backend/internal/user/repository"
+	model "github.com/philipnathan/pijar-backend/internal/user/model"
+	repository "github.com/philipnathan/pijar-backend/internal/user/repository"
 	"github.com/philipnathan/pijar-backend/utils"
 	"gorm.io/gorm"
 )
