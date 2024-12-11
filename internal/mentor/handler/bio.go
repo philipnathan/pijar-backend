a7a113328aba6b997479839da2f4b360ef45aa6d
philipnathan <philipnathan168@gmail.com>
2024-12-11 14:47:31 +0800

    feat(mentor_bio): create read operation for user to get specific mentor bio

--- internal/mentor/handler/bio.go
+++ internal/mentor/handler/bio.go
@@ -2,9 +2,11 @@ package mentor
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	custom_error "github.com/philipnathan/pijar-backend/internal/mentor/custom_error"
+	dto "github.com/philipnathan/pijar-backend/internal/mentor/dto"
 	service "github.com/philipnathan/pijar-backend/internal/mentor/service"
 )
 
@@ -43,4 +45,32 @@ func (h *MentorBioHandler) MentorGetBio(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, bio)
+}
+
+func (h *MentorBioHandler) UserGetBio(c *gin.Context) {
+	MentorIDStr := c.Param("mentor_id")
+	if MentorIDStr == "" {
+		c.JSON(http.StatusBadRequest, custom_error.Error{Error: "mentor_id is required"})
+		return
+	}
+
+	MentorID, err := strconv.ParseUint(MentorIDStr, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, custom_error.Error{Error: "mentor_id is invalid"})
+		return
+	}
+
+	uintMentorID := uint(MentorID)
+
+	mentorBio, err := h.service.GetMentorBio(&uintMentorID)
+	if err != nil {
+		if err == custom_error.ErrMentorBioNotFound {
+			c.JSON(http.StatusOK, dto.GetMentorBioResponseDto{Message: "bio not found", Bio: ""})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, custom_error.Error{Error: err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, dto.GetMentorBioResponseDto{Message: "bio fetched successfully", Bio: mentorBio.Bio})
 }
