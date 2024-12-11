401a19da386d191f62f11904a6ec6cdab7d2bd7b
philipnathan <philipnathan168@gmail.com>
2024-12-11 13:49:03 +0800

    feat(mentor): create database and migration of mentor model

--- database/migrate.go
+++ database/migrate.go
@@ -5,12 +5,13 @@ import (
 
 	category "github.com/philipnathan/pijar-backend/internal/category/model"
 	learner "github.com/philipnathan/pijar-backend/internal/learner/model"
+	mentor "github.com/philipnathan/pijar-backend/internal/mentor/model"
 	user "github.com/philipnathan/pijar-backend/internal/user/model"
 	"gorm.io/gorm"
 )
 
 func MigrateDatabase(db *gorm.DB) {
-	err := db.AutoMigrate(&user.User{}, &category.Category{}, &learner.LearnerBio{}, &learner.LearnerInterest{})
+	err := db.AutoMigrate(&user.User{}, &category.Category{}, &learner.LearnerBio{}, &learner.LearnerInterest{}, &mentor.MentorBiographies{}, &mentor.MentorExperiences{}, &mentor.MentorExpertises{})
 
 	if err != nil {
 		panic(err)
