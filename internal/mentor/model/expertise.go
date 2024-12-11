e841f3bcff02e2a0f0551baf1647e06ccbec2864
philipnathan <philipnathan168@gmail.com>
2024-12-11 20:16:59 +0800

    feat(mentor_expertise): add relationship between mentor expertise and category

--- internal/mentor/model/expertise.go
+++ internal/mentor/model/expertise.go
@@ -1,10 +1,15 @@
 package mentor
 
-import "gorm.io/gorm"
+import (
+	category "github.com/philipnathan/pijar-backend/internal/category/model"
+	"gorm.io/gorm"
+)
 
 type MentorExpertises struct {
 	gorm.Model  `json:"-"`
 	UserID      uint   `gorm:"not null" json:"user_id"`
 	Expertise   string `gorm:"type:varchar(50);not null" json:"expertise"`
 	Category_id uint   `gorm:"not null" json:"category_id"`
+
+	Category category.Category `gorm:"foreignKey:Category_id;references:ID" json:"category"`
 }
