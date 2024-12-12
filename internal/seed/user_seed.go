0d5738e0b6fb024a2c473f7443cf9154b8bd147c
philipnathan <philipnathan168@gmail.com>
2024-12-12 21:22:12 +0800

    fix: hashed user password in seed data

--- internal/seed/user_seed.go
+++ internal/seed/user_seed.go
@@ -1,8 +1,9 @@
 package seed
 
 import (
-	model "github.com/philipnathan/pijar-backend/internal/user/model"
-	repo "github.com/philipnathan/pijar-backend/internal/user/repository"
+	userModel "github.com/philipnathan/pijar-backend/internal/user/model"
+	userRepo "github.com/philipnathan/pijar-backend/internal/user/repository"
+	utils "github.com/philipnathan/pijar-backend/utils"
 	"gorm.io/gorm"
 )
 
@@ -12,26 +13,30 @@ func SeedUser(db *gorm.DB) error {
 		return tx.Error
 	}
 
-	users := []model.User{
+	mentor01PW, _ := utils.HashPassword("mentor01")
+	learner01PW, _ := utils.HashPassword("learner01")
+	learner02PW, _ := utils.HashPassword("learner02")
+
+	users := []userModel.User{
 		{
 			Email:    "mentor01@example.com",
-			Password: "mentor01",
+			Password: mentor01PW,
 			Fullname: "Mentor01",
 		},
 		{
 			Email:    "learner01@example.com",
-			Password: "learner01",
+			Password: learner01PW,
 			Fullname: "learner01",
 		},
 		{
 			Email:    "learner02@example.com",
-			Password: "learner02",
+			Password: learner02PW,
 			Fullname: "learner02",
 		},
 	}
 
 	var count int64
-	if err := db.Model(&model.User{}).Count(&count).Error; err != nil {
+	if err := db.Model(&userModel.User{}).Count(&count).Error; err != nil {
 		return err
 	}
 
@@ -40,7 +45,7 @@ func SeedUser(db *gorm.DB) error {
 	}
 
 	for _, user := range users {
-		if err := repo.NewUserRepository(db).SaveUser(&user); err != nil {
+		if err := userRepo.NewUserRepository(db).SaveUser(&user); err != nil {
 			tx.Rollback()
 			return err
 		}
