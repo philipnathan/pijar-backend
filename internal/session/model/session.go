10f83c7c5b4c4fdbea4c085d20f92af473e7e42e
Ano Jumisa <anojumisa@gmx.de>
2024-12-14 22:30:48 +0800

    bugfix: fix session model relation to user model

--- internal/session/model/session.go
+++ internal/session/model/session.go
@@ -2,11 +2,13 @@ package session
 
 import (
     "time"
+    "github.com/philipnathan/pijar-backend/internal/user/model"
 )
 
 type MentorSession struct {
     ID               uint      `gorm:"primaryKey"`
-    UserID           uint      `gorm:"not null" json:"user_id"`
+    UserID           uint      `gorm:"not null;index" json:"user_id"`
+    User             user.User `gorm:"foreignKey:UserID;references:ID"`
     CategoryID       uint      `gorm:"not null" json:"category_id"`
     Title            string    `gorm:"size:144;not null" json:"title"`
     ShortDescription string    `gorm:"size:256;not null" json:"short_description"`
@@ -20,11 +22,13 @@ type MentorSession struct {
 }
 
 type MentorSessionParticipant struct {
-    ID               uint      `gorm:"primaryKey"`
-    UserID           uint      `gorm:"not null" json:"user_id"`
-    MentorSessionID  uint      `gorm:"not null" json:"mentor_session_id"`
-    Status           string    `gorm:"type:mentor_session_participants_status;default:'registered';not null" json:"status"`
-    Rating           float32   `json:"rating"`
-    RegisteredAt     time.Time `gorm:"autoCreateTime" json:"registered_at"`
-    UpdatedAt        time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+    ID              uint          `gorm:"primaryKey"`
+    MentorSessionID uint          `gorm:"not null;index" json:"mentor_session_id"`
+    MentorSession   MentorSession `gorm:"foreignKey:MentorSessionID;references:ID"`
+    UserID          uint          `gorm:"not null;index" json:"user_id"`
+    User            user.User     `gorm:"foreignKey:UserID;references:ID"`
+    Status          string        `gorm:"type:mentor_session_participants_status;default:'registered';not null" json:"status"`
+    Rating          float32       `json:"rating"`
+    RegisteredAt    time.Time     `gorm:"autoCreateTime" json:"registered_at"`
+    UpdatedAt       time.Time     `gorm:"autoUpdateTime" json:"updated_at"`
 }
