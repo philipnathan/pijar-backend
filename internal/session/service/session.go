02ad8f17c48ade409db84fc43359e42c4ba89514
philipnathan <philipnathan168@gmail.com>
2024-12-17 17:00:19 +0800

    feat(session): change package name

--- internal/session/service/session.go
+++ internal/session/service/session.go
@@ -1,28 +1,28 @@
-package service
+package session
 
 import (
-    model "github.com/philipnathan/pijar-backend/internal/session/model"
-    repository "github.com/philipnathan/pijar-backend/internal/session/repository"
+	model "github.com/philipnathan/pijar-backend/internal/session/model"
+	repository "github.com/philipnathan/pijar-backend/internal/session/repository"
 )
 
 type SessionService interface {
-    GetSessions(userID uint) ([]model.MentorSession, error)
+	GetSessions(userID uint) ([]model.MentorSession, error)
 }
 
 type sessionService struct {
-    repo repository.SessionRepository
+	repo repository.SessionRepository
 }
 
 func NewSessionService(repo repository.SessionRepository) SessionService {
-    return &sessionService{repo: repo}
+	return &sessionService{repo: repo}
 }
 
 func (s *sessionService) GetSessions(userID uint) ([]model.MentorSession, error) {
-    // Fetch sessions from the repository
-    sessions, err := s.repo.GetSessions(userID)
-    if err != nil {
-        return nil, err
-    }
+	// Fetch sessions from the repository
+	sessions, err := s.repo.GetSessions(userID)
+	if err != nil {
+		return nil, err
+	}
 
-    return sessions, nil
+	return sessions, nil
 }
