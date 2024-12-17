8fb109f4551e4f9873dfc0e50db021f7c4356dfe
philipnathan <philipnathan168@gmail.com>
2024-12-17 16:59:25 +0800

    feat(session): change package name & add preload to load user while fetch sessions data

--- internal/session/repository/session.go
+++ internal/session/repository/session.go
@@ -1,38 +1,40 @@
-package repository
+package session
 
 import (
-    model "github.com/philipnathan/pijar-backend/internal/session/model"
-    "gorm.io/gorm"
 	time "time"
+
+	model "github.com/philipnathan/pijar-backend/internal/session/model"
+	"gorm.io/gorm"
 )
 
 type SessionRepository interface {
-    GetSessions(userID uint) ([]model.MentorSession, error)
-    FetchUpcomingSessions(userID uint) ([]model.MentorSession, error)
+	GetSessions(userID uint) ([]model.MentorSession, error)
+	FetchUpcomingSessions(userID uint) ([]model.MentorSession, error)
 }
 
 type sessionRepository struct {
-    db *gorm.DB
+	db *gorm.DB
 }
 
 func NewSessionRepository(db *gorm.DB) SessionRepository {
-    return &sessionRepository{db: db}
+	return &sessionRepository{db: db}
 }
 
 func (r *sessionRepository) GetSessions(userID uint) ([]model.MentorSession, error) {
-    var sessions []model.MentorSession
-    err := r.db.Where("user_id = ?", userID).Find(&sessions).Error
-    if err != nil {
-        return nil, err
-    }
-    return sessions, nil
+	var sessions []model.MentorSession
+	err := r.db.Preload("User").Where("user_id = ?", userID).Find(&sessions).Error
+	if err != nil {
+		return nil, err
+	}
+	return sessions, nil
 }
 
 func (r *sessionRepository) FetchUpcomingSessions(userID uint) ([]model.MentorSession, error) {
-    var sessions []model.MentorSession
-    err := r.db.Where("user_id = ? AND schedule > ?", userID, time.Now()).Find(&sessions).Error
-    if err != nil {
-        return nil, err
-    }
-    return sessions, nil
+	var sessions []model.MentorSession
+	err := r.db.Preload("User").Where("user_id = ? AND schedule > ?", userID, time.Now()).Find(&sessions).Error
+
+	if err != nil {
+		return nil, err
+	}
+	return sessions, nil
 }
