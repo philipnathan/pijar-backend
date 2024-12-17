3aa389ebb914b043e07468293d7b03827f830dee
philipnathan <philipnathan168@gmail.com>
2024-12-17 17:50:28 +0800

    feat(session): create documentation for mentor session

--- internal/session/dto/get_upcoming_session.go
+++ internal/session/dto/get_upcoming_session.go
@@ -1,15 +1,14 @@
-package dto
-
+package session
 
 type GetUpcomingSessionResponse struct {
 	Sessions []SessionDetail `json:"sessions"`
 }
 
 type SessionDetail struct {
-	Day                string    `json:"day"`                  
-	Time               string    `json:"time"`                 
-	MentorSessionTitle string    `json:"mentor_session_title"` 
-	ShortDescription   string    `json:"short_description"`    
-	Schedule           string    `json:"schedule"`             
-	Registered         bool      `json:"registered"`           
+	Day                string `json:"day"`
+	Time               string `json:"time"`
+	MentorSessionTitle string `json:"mentor_session_title"`
+	ShortDescription   string `json:"short_description"`
+	Schedule           string `json:"schedule"`
+	Registered         bool   `json:"registered"`
 }
