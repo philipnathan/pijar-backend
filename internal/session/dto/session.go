ae41343ff4c29d7792b359f9fb2f1449b96a770d
philipnathan <philipnathan168@gmail.com>
2024-12-17 17:39:14 +0800

    Feat(session): arrange response field

--- internal/session/dto/session.go
+++ internal/session/dto/session.go
@@ -1,14 +1,21 @@
-package dto
+package session
+
+import "time"
 
 type MentorSessionResponse struct {
-    Day                string `json:"day"`
-    Time               string `json:"time"`
-    MentorSessionTitle string `json:"mentor_session_title"`
-    ShortDescription   string `json:"short_description"`
-    Schedule           string `json:"schedule"`
-    Registered         bool   `json:"registered"`
+	MentorSessionTitle string        `json:"mentor_session_title"`
+	ShortDescription   string        `json:"short_description"`
+	Schedule           time.Time     `json:"schedule"`
+	Registered         bool          `json:"registered"`
+	MentorDetails      MentorDetails `json:"mentor_details"`
 }
 
 type GetUpcomingMentorSessionResponse struct {
-    Sessions []MentorSessionResponse `json:"sessions"`
+	Sessions []MentorSessionResponse `json:"sessions"`
+}
+
+type MentorDetails struct {
+	Id       uint   `json:"id" example:"1"`
+	Fullname string `json:"fullname" example:"John Doe"`
+	ImageURL string `json:"image_url" example:"https://example.com/image.jpg"`
 }
