45f33453235bd70bf630ab0b014954b10ad1f7b1
philipnathan <philipnathan168@gmail.com>
2025-01-16 22:45:32 +0800

    refactor(session): refactor response for all handler from session entity

--- internal/session/dto/get_all_sessions.go
+++ internal/session/dto/get_all_sessions.go
@@ -9,15 +9,19 @@ type GetAllSessionsResponse struct {
 
 type Session struct {
 	ID               uint          `json:"session_id"`
-	Day              string        `json:"day"`
-	Time             string        `json:"time"`
+	MentorDetails    MentorDetails `json:"mentor_details"`
+	Category         string        `json:"category"`
 	Title            string        `json:"title"`
 	ShortDescription string        `json:"short_description"`
-	ImageURL         string        `json:"image_url"`
+	Detail           string        `json:"detail"`
 	Schedule         string        `json:"schedule"`
-	MentorDetails    MentorDetails `json:"mentor_details"`
-	AverageRating    float32       `json:"average_rating"`
 	Duration         int           `json:"duration"`
+	ImageURL         string        `json:"image_url"`
+	Link             string        `json:"link"`
+
+	Day           string  `json:"day"`
+	Time          string  `json:"time"`
+	AverageRating float32 `json:"average_rating"`
 }
 
 type MentorDetails struct {
