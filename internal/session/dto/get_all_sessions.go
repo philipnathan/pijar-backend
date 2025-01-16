4a0a0678cc20781d756be5e184a063d501e7d2fb
philipnathan <philipnathan168@gmail.com>
2025-01-16 22:27:01 +0800

    refactor(session): refactor response for all handler from session entity and fix the documentation

--- internal/session/dto/get_all_sessions.go
+++ internal/session/dto/get_all_sessions.go
@@ -1,9 +1,5 @@
 package session
 
-import (
-	"time"
-)
-
 type GetAllSessionsResponse struct {
 	Sessions []Session `json:"sessions"`
 	Total    int       `json:"total"`
@@ -12,10 +8,20 @@ type GetAllSessionsResponse struct {
 }
 
 type Session struct {
-	MentorSessionTitle string        `json:"mentor_session_title"`
-	ShortDescription   string        `json:"short_description"`
-	ImageURL           string        `json:"image_url"`
-	Schedule           time.Time     `json:"schedule"`
-	MentorDetails      MentorDetails `json:"mentor_details"`
-	AverageRating      float32       `json:"average_rating"`
+	ID               uint          `json:"session_id"`
+	Day              string        `json:"day"`
+	Time             string        `json:"time"`
+	Title            string        `json:"title"`
+	ShortDescription string        `json:"short_description"`
+	ImageURL         string        `json:"image_url"`
+	Schedule         string        `json:"schedule"`
+	MentorDetails    MentorDetails `json:"mentor_details"`
+	AverageRating    float32       `json:"average_rating"`
+	Duration         int           `json:"duration"`
+}
+
+type MentorDetails struct {
+	Id       uint   `json:"id" example:"1"`
+	Fullname string `json:"fullname" example:"John Doe"`
+	ImageURL string `json:"image_url" example:"https://example.com/image.jpg"`
 }
