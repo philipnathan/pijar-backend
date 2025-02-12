eb9c7bdc56c58c3c75697b003aab408c77bfa981
philipnathan <philipnathan168@gmail.com>
2025-02-12 14:04:43 +0800

    feat(notification): implement dependency injection using google wire

--- internal/notification/route/notification.go
+++ internal/notification/route/notification.go
@@ -1,21 +1,20 @@
 package notification
 
 import (
-	handler "github.com/philipnathan/pijar-backend/internal/notification/handler"
-	repository "github.com/philipnathan/pijar-backend/internal/notification/repository"
-	service "github.com/philipnathan/pijar-backend/internal/notification/service"
 	middleware "github.com/philipnathan/pijar-backend/middleware"
 	"gorm.io/gorm"
 
 	"github.com/gin-gonic/gin"
+	initNotification "github.com/philipnathan/pijar-backend/internal/notification"
 )
 
 func NotificationRoute(r *gin.Engine, db *gorm.DB) {
 	apiV1 := "/api/v1/users/notifications"
 
-	repo := repository.NewNotificationRepository(db)
-	services := service.NewNotificationService(repo)
-	handler := handler.NewNotificationHandler(services)
+	handler, err := initNotification.InitializedNotification(db)
+	if err != nil {
+		panic(err)
+	}
 
 	protectedRoutes := r.Group(apiV1)
 	{
