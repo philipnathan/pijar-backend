7a3816e59f5ccad209c263ee451bbd0f43e31018
philipnathan <philipnathan168@gmail.com>
2025-02-12 12:00:30 +0800

    feat(mentor_session_participant): implement dependency injection using google wire

--- internal/mentor_session_participant/route/mentor_session_participant.go
+++ internal/mentor_session_participant/route/mentor_session_participant.go
@@ -2,14 +2,7 @@ package mentor_session_participant
 
 import (
 	"github.com/gin-gonic/gin"
-	learnerRepo "github.com/philipnathan/pijar-backend/internal/learner/repository"
-	handler "github.com/philipnathan/pijar-backend/internal/mentor_session_participant/handler"
-	repo "github.com/philipnathan/pijar-backend/internal/mentor_session_participant/repository"
-	service "github.com/philipnathan/pijar-backend/internal/mentor_session_participant/service"
-	sessionRepository "github.com/philipnathan/pijar-backend/internal/session/repository"
-	sessionService "github.com/philipnathan/pijar-backend/internal/session/service"
-	userRepository "github.com/philipnathan/pijar-backend/internal/user/repository"
-	userService "github.com/philipnathan/pijar-backend/internal/user/service"
+	initMentorSession "github.com/philipnathan/pijar-backend/internal/mentor_session_participant"
 	middleware "github.com/philipnathan/pijar-backend/middleware"
 	"gorm.io/gorm"
 )
@@ -17,16 +10,10 @@ import (
 func MentorSessionParticipantRoute(r *gin.Engine, db *gorm.DB) {
 	apiV1 := "/api/v1/sessions/"
 
-	repo := repo.NewMentorSessionParticipantRepository(db)
-	userRepo := userRepository.NewUserRepository(db)
-	sessionRepo := sessionRepository.NewSessionRepository(db)
-	learnerRepo := learnerRepo.NewLearnerRepository(db)
-	srv := service.NewMentorSessionParticipantService(
-		repo,
-		userService.NewUserService(userRepo),
-		sessionService.NewSessionService(sessionRepo, learnerRepo),
-	)
-	hnd := handler.NewMentorSessionParticipantHandler(srv)
+	hnd, err := initMentorSession.InitializedMentorSessionParticipant(db)
+	if err != nil {
+		panic(err)
+	}
 
 	protectedRoutes := r.Group(apiV1)
 	{
